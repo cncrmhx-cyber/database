8c7fb9667dd7ce1b8de4b98450e3a2b76b525924
cncrmhx-cyber <cncrmhx@gmail.com>
2025-10-17 17:57:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-import express from "express";
-import http from "http";
-import { Server } from "socket.io";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(join(__dirname, "public")));
+// Servir les fichiers statiques (index.html)
+app.use(express.static(__dirname));
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+const rooms = {};
 
-  socket.on("join", (room) => {
+io.on('connection', socket => {
+  console.log('Nouvel utilisateur connecté');
+
+  socket.on('join', room => {
     socket.join(room);
-    socket.to(room).emit("new-user", socket.id);
-  });
+    if (!rooms[room]) rooms[room] = [];
+    rooms[room].push(socket.id);
 
-  socket.on("signal", ({ room, data, to }) => {
-    socket.to(to || room).emit("signal", { from: socket.id, data });
-  });
+    // Notifier les autres dans la room
+    socket.to(room).emit('new-user', socket.id);
 
-  socket.on("disconnect", () => {
-    io.emit("user-left", socket.id);
+    // Quand un utilisateur se déconnecte
+    socket.on('disconnect', () => {
+      rooms[room] = rooms[room].filter(id => id !== socket.id);
+      socket.to(room).emit('user-left', socket.id);
+    });
+
+    // Signaling
+    socket.on('signal', ({ to, data }) => {
+      io.to(to).emit('signal', { from: socket.id, data });
+    });
   });
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => console.log("Server running on port", port));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
